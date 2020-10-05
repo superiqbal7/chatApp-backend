df3fe8e41adbca961a632501843806ce017235fe
rahim iqbal <superiqu@gmail.com>
2020-10-05 17:37:31 +0600

    fix findbyusername route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,6 +7,6 @@ const AuthHelper = require('../Helper/AuthHelper');
 
 router.get('/users', AuthHelper.VerifyToken, UserCtrl.GetAllUsers);
 router.get('/user/:id', AuthHelper.VerifyToken, UserCtrl.GetUsers);
-router.get('/user/:username', AuthHelper.VerifyToken, UserCtrl.GetUserByUserName);
+router.get('/username/:username', AuthHelper.VerifyToken, UserCtrl.GetUserByUserName);
 
 module.exports = router;
