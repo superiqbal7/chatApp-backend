ec3f0bfad84388807d0eb0df17f9e3f529353359
rahim iqbal <superiqu@gmail.com>
2020-10-07 18:12:08 +0600

    add markallnotification functionality to update db

--- controllers/friends.js
+++ controllers/friends.js
@@ -134,5 +134,24 @@ module.exports = {
             .json({ message: 'Error occured' });
         });
     }
+  },
+
+  async MarkAllNotifications(req, res){
+    await User.update({
+      _id: req.user._id
+    },{
+      $set: { 'notifications.$[elem].read': true}
+    },{
+      arrayFilters: [{'elem.read':false}], multi: true
+    })
+      .then(() => {
+        res.status(HttpStatus.OK).json({ message: 'Marked all Successfully' });
+      })
+      .catch(err => {
+        res
+          .status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .json({ message: 'Error occured' });
+      });
   }
+
 };
