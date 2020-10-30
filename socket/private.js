12d99e6e44fee704cfe3f79f3608b31d4f1a99dc
rahim iqbal <superiqu@gmail.com>
2020-10-31 01:04:46 +0600

    user start type and stop type tracking in chat

---
+++ socket/private.js
@@ -0,0 +1,19 @@
+
+module.exports = function (io) {
+  io.on('connection', (socket) => {
+    socket.on('join chat', params => {
+      console.log("xcgzxv");
+      //io.emit('refreshPage', {})
+      socket.join(params.room1);
+      socket.join(params.room2);
+    });
+
+    socket.on('start_typing', data => {
+      io.to(data.receiver).emit('is_typing', data);
+    })
+
+    socket.on('stop_typing', data => {
+      io.to(data.receiver).emit('stopped_typing', data);
+    })
+  })
+}
