9aeb5272bf7273af12e2ef1a74a811a357e4c7a2
rahim iqbal <superiqu@gmail.com>
2020-10-31 01:04:20 +0600

    add messge get router

--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -5,6 +5,8 @@ const router = express.Router();
 const MessageCtrl = require('../controllers/message.js');
 const AuthHelper = require('../Helper/AuthHelper');
 
+router.get('/chat-messages/:sender_Id/:receiver_Id', AuthHelper.VerifyToken, MessageCtrl.GetAllMessages);
+
 router.post('/chat-messages/:sender_Id/:receiver_Id', AuthHelper.VerifyToken, MessageCtrl.SendMessage);
 
 
