4a79fcfda5d08e693f536b6cd973ccb33cdca9f4
rahim iqbal <superiqu@gmail.com>
2020-11-01 00:17:19 +0600

    refactor code

--- server.js
+++ server.js
@@ -11,24 +11,32 @@ const dbConfig = require("./config/secret");
 
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
+require('./socket/streams')(io);
+require('./socket/private')(io);
 
-app.use((req, res, next) => {
-  res.header("Access-Controll-Allow-Origin", "*");
-  res.header("Access-Controll-Allow-Credentials", "true");
-  res.header(
-    "Access-Controll-Allow-Methods",
-    "GET",
-    "POST",
-    "DELETE",
-    "PUT",
-    "OPTIONS"
-  );
-  res.header(
-    "Access-Controll-Allow-Headers",
-    "Origin, X-Requested-with, Content-Type, Accept, Authorization"
-  );
-  next();
-});
+const auth = require("./routes/authRoutes");
+const posts = require("./routes/postRoutes");
+const users = require("./routes/userRoute");
+const friends = require('./routes/friendsRoutes');
+const message = require('./routes/messageRouter');
+
+// app.use((req, res, next) => {
+//   res.header("Access-Controll-Allow-Origin", "*");
+//   res.header("Access-Controll-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Controll-Allow-Methods",
+//     "GET",
+//     "POST",
+//     "DELETE",
+//     "PUT",
+//     "OPTIONS"
+//   );
+//   res.header(
+//     "Access-Controll-Allow-Headers",
+//     "Origin, X-Requested-with, Content-Type, Accept, Authorization"
+//   );
+//   next();
+// });
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
@@ -42,14 +50,6 @@ mongoose.connect(dbConfig.url, {
   useUnifiedTopology: true,
 });
 
-require('./socket/streams')(io);
-require('./socket/private')(io);
-
-const auth = require("./routes/authRoutes");
-const posts = require("./routes/postRoutes");
-const users = require("./routes/userRoute");
-const friends = require('./routes/friendsRoutes');
-const message = require('./routes/messageRouter');
 //const { REQUEST_TIMEOUT } = require("http-status-codes");
 
 app.use("/api/chatapp", auth);
