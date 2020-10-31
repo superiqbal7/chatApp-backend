afae1810a3039971b1e516ddf98da8240ed88ce7
rahim iqbal <superiqu@gmail.com>
2020-11-01 00:17:08 +0600

    update chatlist to recent message

--- Helper/helpers.js
+++ Helper/helpers.js
@@ -1,3 +1,5 @@
+const User = require('../models/userModel');
+
 module.exports = {
     firstUpper: (username) => {
         const name = username.toLowerCase();
@@ -6,5 +8,66 @@ module.exports = {
 
     lowerCase: str => {
         return str.toLowerCase();
+    },
+
+    updatechatList: async (req, message) => {
+        await User.update(
+            {
+                _id: req.user._id
+            },
+            {
+                $pull: {
+                    chatList: {
+                        receiverId: req.params.receiver_Id
+                    }
+                }
+            }
+        );
+
+        await User.update(
+            {
+                _id: req.params.receiver_Id
+            },
+            {
+                $pull: {
+                    chatList: {
+                        receiverId: req.user._id
+                    }
+                }
+            }
+        );
+
+        await User.updateOne({
+            _id: req.user._id
+        }, {
+            $push: {
+                chatList: {
+                    $each: [
+                        {
+                            receiverId: req.params.receiver_Id,
+                            msgId: message._id
+                        }
+                    ],
+                    $position: 0
+                }
+            }
+        });
+
+        await User.updateOne({
+            _id: req.params.receiver_Id
+        }, {
+            $push: {
+                chatList: {
+                    $each: [
+                        {
+                            receiverId: req.user._id,
+                            msgId: message._id
+                        }
+                    ],
+                    $position: 0
+                }
+            }
+        });
+        
     }
-}
+};
