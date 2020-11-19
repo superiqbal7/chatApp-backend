1b54607a2009a6e497818b0fe9c5ef219c51735f
rahim iqbal <superiqu@gmail.com>
2020-11-19 23:38:38 +0600

    add set default image route

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -7,4 +7,6 @@ const AuthHelper = require('../Helper/AuthHelper');
 
 router.post('/upload-image', AuthHelper.VerifyToken, ImageCtrl.UploadImage);
 
+router.get('/set-default-image/:imgId/:imgVersion', AuthHelper.VerifyToken, ImageCtrl.SetDefaultImage)
+
 module.exports = router;
